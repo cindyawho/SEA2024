1c93e6c615a07ef4b4a445a58a3828d5d5bd79a2
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-04-05 17:58:14 -0700

    Added data of books I've read this year. Changed numbers to ints instead of strings
    I'm currently reading Y:The Last Man but hope to finish it soon! :)

--- data.js
+++ data.js
@@ -4,19 +4,153 @@ books =
 {
     "title" : "Percy Jackson: The Lightning Thief",
     "author" : "Rick Riordan",
-    "pages" : "377",
+    "pages" : 377,
     "genre" : ["Fantasy", "Young Adult", "Mythology", "Middle Grade"],
-    "yearOfPub" : "2005",
-    "rating" : "5",
-    "url" : "https://www.goodreads.com/book/show/28187.The_Lightning_Thief?ref=nav_sb_ss_1_7"
+    "yearOfPub" : 2005,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/28187.The_Lightning_Thief?ref=nav_sb_ss_1_7",
+    "image" : "https://i0.wp.com/books.disney.com/content/uploads/2013/09/9780786856299.jpg?fit=405%2C612&ssl=1"
 },
 {
     "title" : "Clytemnestra",
     "author" : "Costanza Casati",
-    "pages" : "448",
+    "pages" : 448,
     "genre" : ["Mythology", "Fantasy", "Historical Fiction"],
-    "yearOfPub" : "2023",
-    "rating" : "4",
-    "url" : "https://www.goodreads.com/book/show/61361681-clytemnestra?ac=1&from_search=true&qid=LufubZ9FXC&rank=1"
+    "yearOfPub" : 2023,
+    "rating" : 4,
+    "url" : "https://www.goodreads.com/book/show/61361681-clytemnestra?ac=1&from_search=true&qid=LufubZ9FXC&rank=1",
+    "image" : "https://dynamic.indigoimages.ca/v1/books/books/1443470384/1.jpg?width=614&maxheight=614&quality=85"
+},
+{
+    "title" : "The Chalice of the Gods",
+    "author" : "Rick Riordan",
+    "pages" : 268,
+    "genre" : ["Fantasy", "Mythology", "Young Adult"],
+    "yearOfPub" : 2023,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/63035432-the-chalice-of-the-gods",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1677259920i/63035432.jpg"
+},
+{
+    "title" : "Psyche and Eros",
+    "author" : "Luna McNamara",
+    "pages" : 352,
+    "genre" : ["Mythology", "Fantasy", "Romance"],
+    "yearOfPub" : 2023,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/62292422-psyche-and-eros",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1675646235i/62292422.jpg"
+},
+{
+    "title" : "Dreamland: The True Tale of America's Opiate Epidemic",
+    "author" : "Sam Quinones",
+    "pages" : 384,
+    "genre" : ["Nonfiction", "History", "True Crime", "Politics"],
+    "yearOfPub" : 2015,
+    "rating" : 4,
+    "url" : "https://www.goodreads.com/book/show/22529381-dreamland",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1421965034i/22529381.jpg"
+},
+{
+    "title" : "Chinese Cinderella: The True Story of an Unwanted Daughter",
+    "author" : "Adeline Yen Mah",
+    "pages" : 240,
+    "genre" : ["Nonfiction", "Memoir", "Biography"],
+    "yearOfPub" : 1999,
+    "rating" : 4,
+    "url" : "https://www.goodreads.com/book/show/54579611-chinese-cinderella",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1595084879i/54579611.jpg"
+},
+{
+    "title" : "Kindred",
+    "author" : "Octavia E. Butler",
+    "pages" : 288,
+    "genre" : ["Historical Fiction", "Fiction", "Science Fiction"],
+    "yearOfPub" : 1979,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/60931.Kindred",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1339423248i/60931.jpg"
+},
+{
+    "title" : "Daughters of Sparta",
+    "author" : "Claire Heywood",
+    "pages" : 370,
+    "genre" : ["Mythology", "Historical Fiction"],
+    "yearOfPub" : 2021,
+    "rating" : 3,
+    "url" : "https://www.goodreads.com/book/show/57423631-daughters-of-sparta",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1616478164i/57423631.jpg"
+},
+{
+    "title" : "Adventure in Greece: A Bilingual Story for Advanced Beginners to Intermediate Level",
+    "author" : "Athina Veloudou, Hugh Finlayson",
+    "pages" : 50,
+    "genre" : ["Fiction", "Language"],
+    "yearOfPub" : 2022,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/61366605-adventure-in-greece",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1656451218i/61366605.jpg"
+},
+{
+    "title" : "Lady Tan’s Circle of Women",
+    "author" : "Lisa See",
+    "pages" : 368,
+    "genre" : ["Historical Fiction", "Fiction", "Historical", "China"],
+    "yearOfPub" : 2023,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/62919732-lady-tan-s-circle-of-women",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1683735261i/62919732.jpg"
+},
+{
+    "title" : "Mexican Gothic",
+    "author" : "Silva-Moreno Garcia",
+    "pages" : 320,
+    "genre" : ["Horror", "Fiction", "Historical Fiction", "Gothic", "Mystery"],
+    "yearOfPub" : 2020,
+    "rating" : 4,
+    "url" : "https://www.goodreads.com/book/show/53152636-mexican-gothic",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1607462569i/53152636.jpg"
+},
+{
+    "title" : "Can't We Talk about Something More Pleasant?",
+    "author" : "Roz Chast",
+    "pages" : 228,
+    "genre" : ["Graphic Novels", "Memoir"],
+    "yearOfPub" : 2014,
+    "rating" : 4,
+    "url" : "https://www.goodreads.com/book/show/18594409-can-t-we-talk-about-something-more-pleasant",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1421087235i/18594409.jpg"
+},
+{
+    "title" : "6 Times We Almost Kissed [and One Time We Did]",
+    "author" : "Tess Sharpe",
+    "pages" : 384,
+    "genre" : ["Romance", "Lesbian", "Young Adult"],
+    "yearOfPub" : 2023,
+    "rating" : 4,
+    "url" : "https://www.goodreads.com/book/show/61030513-6-times-we-almost-kissed-and-one-time-we-did",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1652690535i/61030513.jpg"
+},
+{
+    "title" : "Y: The Last Man Omnibus",
+    "author" : "Brian K. Vaughan",
+    "pages" : 1424,
+    "genre" : ["Comics", "Graphic Novels", "Science Fiction"],
+    "yearOfPub" : 2008,
+    "rating" : 5,
+    "url" : "https://www.goodreads.com/book/show/44512946-y",
+    "image" : "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1572996287i/44512946.jpg"
 }
 ];
+
+//Template for new addition to data
+// {
+//     "title" : "abc",
+//     "author" : "abc",
+//     "pages" : 123,
+//     "genre" : ["abc"],
+//     "yearOfPub" : 123,
+//     "rating" : 123,
+//     "url" : "abc",
+//     "image" : "abc"
+// },
